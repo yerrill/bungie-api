2e7c027bda381009df163d34e057d2156aead2e1
Zach Yerrill <26354308+yerrill@users.noreply.github.com>
2021-03-29 00:31:13 -0400

    Small Refactor

--- index.js
+++ index.js
@@ -2,44 +2,56 @@ const https = require('https')
 const fs = require("fs")
 
 // Load API key from secrets file
-var key = null
+var apikey = null
 
 fs.readFile('secrets.json', 'utf-8', (err, data) => {
   if (err) throw err;
-  key = JSON.parse(data)["key"];
+  apikey = JSON.parse(data)["key"];
 });
 
 
 // Make API request
-const API_BASE_ENDPOINT = 'www.bungie.net'
-const API_ENPOINT_PATH = '/Platform'
-
-const data = JSON.stringify({
-  'X-API-Key': key
-})
-
-const options = {
-  hostname: API_BASE_ENDPOINT,
-  port: 443,
-  path: API_ENPOINT_PATH + '/Destiny2/Manifest/',
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-    'Content-Length': data.length
+function API_request(req_path, req_method, key){
+  const API_BASE_ENDPOINT = 'www.bungie.net'
+  const API_ENPOINT_PATH = '/Platform'
+  
+  const data = JSON.stringify({
+    'X-API-Key': key
+  })
+  
+  const options = {
+    hostname: API_BASE_ENDPOINT,
+    port: 443,
+    path: API_ENPOINT_PATH + req_path,
+    method: req_method,
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length
+    }
   }
+  
+  const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`)
+  
+    res.on('data', d => {
+      process.stdout.write(d)
+    })
+  })
+  
+  req.on('error', error => {
+    console.error(error)
+  })
+  
+  req.write(data)
+  req.end()
 }
 
-const req = https.request(options, res => {
-  console.log(`statusCode: ${res.statusCode}`)
+function Destiny2_GetDestinyManifest(){
+  API_request('/Destiny2/Manifest/', 'GET', apikey)
+}
 
-  res.on('data', d => {
-    process.stdout.write(d)
-  })
-})
+//Destiny2_GetDestinyManifest()
 
-req.on('error', error => {
-  console.error(error)
-})
-
-req.write(data)
-req.end()
+function Destiny2_SearchDestinyPlayer(){
+  
+}
