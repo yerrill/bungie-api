24022bcd6896d38fc7a2e0c3b46756f9e5fee48d
Zach Yerrill <26354308+yerrill@users.noreply.github.com>
2021-03-29 11:59:22 -0400

    Fixed API header, refactor, changed key load to synchronous load

--- index.js
+++ index.js
@@ -1,32 +1,21 @@
 const https = require('https')
 const fs = require("fs")
 
-// Load API key from secrets file
-var apikey = null
-
-fs.readFile('secrets.json', 'utf-8', (err, data) => {
-  if (err) throw err;
-  apikey = JSON.parse(data)["key"];
-});
-
+// Load API key from secrets file (synchonously)
+const apikey = JSON.parse(fs.readFileSync('./secrets.json', 'utf-8'))["key"]
 
 // Make API request
-function API_request(req_path, req_method, key){
+function API_GET(req_path, key){
   const API_BASE_ENDPOINT = 'www.bungie.net'
   const API_ENPOINT_PATH = '/Platform'
-  
-  const data = JSON.stringify({
-    'X-API-Key': key
-  })
   
   const options = {
     hostname: API_BASE_ENDPOINT,
     port: 443,
     path: API_ENPOINT_PATH + req_path,
-    method: req_method,
+    method: 'GET',
     headers: {
-      'Content-Type': 'application/json',
-      'Content-Length': data.length
+      'X-API-Key': key
     }
   }
   
@@ -41,17 +30,24 @@ function API_request(req_path, req_method, key){
   req.on('error', error => {
     console.error(error)
   })
-  
-  req.write(data)
+
   req.end()
 }
 
 function Destiny2_GetDestinyManifest(){
-  API_request('/Destiny2/Manifest/', 'GET', apikey)
+  // https://bungie-net.github.io/multi/operation_get_Destiny2-GetDestinyManifest.html#operation_get_Destiny2-GetDestinyManifest
+
+  API_GET('/Destiny2/Manifest/', apikey);
 }
 
 //Destiny2_GetDestinyManifest()
 
-function Destiny2_SearchDestinyPlayer(){
-  
-}
+function Destiny2_SearchDestinyPlayer(membershipType, displayName, original){
+  // https://bungie-net.github.io/multi/operation_get_Destiny2-SearchDestinyPlayer.html#operation_get_Destiny2-SearchDestinyPlayer
+  // /Destiny2/SearchDestinyPlayer/{membershipType}/{displayName}/
+
+  API_GET(`/Destiny2/SearchDestinyPlayer/${membershipType}/${displayName}/?returnOriginalProfile=${original}`, apikey);
+};
+
+console.log(apikey)
+Destiny2_SearchDestinyPlayer('-1', 'Prophet', 'false')
